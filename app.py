11a12d9c615c48f4185dbe0ff08322712a109bad
deog.amani@gmail.com <deog.amani@gmail.com>
2020-12-17 13:47:43 +0200

    working

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+st.title("Flight Delaying Detector")
+    # st.subheader("ML App with Streamlit")
+html_temp = """
+    <div style="background-color:blue;padding:10px">
+    <h1 style="color:white;text-align:center;">Streamlit ML App </h1>
+    </div>
+    """
+st.markdown(html_temp,unsafe_allow_html=True)
+
+model=pickle.load(open('model.pkl','rb'))        
+MONTH= st.text_input("MONTH","Type Here",key='1')
+DAY_OF_MONTH= st.text_input("DAY_OF_MONTH","Type Here",key='2')
+DAY_OF_WEEK=st.text_input("DAY_OF_WEEK","Type Here",key='3')
+CRS_DEP_TIME= st.text_input("CRS_DEP_TIME","Type Here",key='4')
+ORIGIN= st.text_input("ORIGIN","Type Here",key='5')
+DEST= st.text_input("DEST","Type Here",key='6')
+DISTANCE= st.text_input("DISTANCE","Type Here",key='7')
+Carriermeandistance= st.text_input("Carrier mean distance","Type Here",key='8')
+OriginDelay= st.text_input("Origin Delay","Type Here",key='9')
+OriginTaxiOut= st.text_input("Origin TaxiOut","Type Here",key='10')
+if st.button("predict"):
+    input=np.array([[MONTH,DAY_OF_MONTH,DAY_OF_WEEK, CRS_DEP_TIME,ORIGIN,DEST,DISTANCE,Carriermeandistance,OriginDelay,OriginTaxiOut]]).astype(np.float64)
+    prediction=model.predict(input)
+    st.write(prediction)
+    
+    if prediction == 1:
+        st.warning("The flight is going to delay")
+    else :
+        st.success("The flight is not going to delay")
